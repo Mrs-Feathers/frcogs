4042d55c00afbf53ccb602a2456a49acc30a63ea
Mrs Feathers <echo@furryrefuge.com>
2024-04-04 16:38:36 -0400

    Update __init__.py

--- frid/__init__.py
+++ frid/__init__.py
@@ -5,6 +5,3 @@ __red_end_user_data_statement__ = "This cog allows you to request your FR ID car
 
 async def setup(bot):
     await bot.add_cog(FRIDPlugin(bot))
-    class FRIDCog(commands.Cog):
-        def __init__(self, bot):
-            self.bot = bot
