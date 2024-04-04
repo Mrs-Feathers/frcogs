1b03244ea09c99cf203fdf30b40429e420485959
Mrs Feathers <echo@furryrefuge.com>
2024-04-04 16:07:33 -0400

    Update __init__.py

--- frid/__init__.py
+++ frid/__init__.py
@@ -1,7 +1,7 @@
-from .achievements import Achievements
+from .frid import FRIDPlugin
 
 __red_end_user_data_statement__ = "This cog allows you to request your FR ID card using information from your Furry Refuge account!"
 
 
 async def setup(bot):
-    await bot.add_cog(Achievements(bot))
+    await bot.add_cog(FRIDPlugin(bot))
