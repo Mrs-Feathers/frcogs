0292544293b832f5f8f74f483594b20469cd1aed
Mrs Feathers <echo@furryrefuge.com>
2024-04-29 00:51:44 -0400

    Update getuser.py

--- getuser/getuser.py
+++ getuser/getuser.py
@@ -15,6 +15,44 @@ class GetUser(commands.Cog):
         self.bot = bot
 
     @commands.command(name="getuser")
-    async def signup(self, ctx):
-        """Deliver Invite link."""
-        await ctx.send("Your furry refuge account invite link is: https://auth.furryrefuge.com/if/flow/enrollment-invitation/?itoken=62bd2e38-b67b-46d2-9118-7636d54d48c6", delete_after=10)
+    async def get_user(self, ctx, platform, username):
+        """Fetch user information from different platforms."""
+        if platform in ["tg", "telegram"]:
+            username = f"@{username}" if not username.startswith('@') else username
+            platform = "tgname"
+            url = f"https://auth.furryrefuge.com/api/v3/core/users/?attributes=%7B%22{platform}%22%3A+%22{username}%22%7D"
+        elif platform in ["disc", "discord"]:
+            platform = "discname"
+            url = f"https://auth.furryrefuge.com/api/v3/core/users/?attributes=%7B%22{platform}%22%3A+%22{username}%22%7D"
+        elif platform == "fr":
+            url = f"https://auth.furryrefuge.com/api/v3/core/users/?attributes=%7B%22tgname%22%3A+%22%40Mrs_Feathers%22%7D&username={username}"
+        else:
+            await ctx.send('Please specify a valid platform: "tg" for Telegram, "disc" for Discord, or "fr" for Furry Refuge.')
+            return
+
+        headers = {
+            'accept': 'application/json',
+            'authorization': 'Bearer ***REMOVED***'
+        }
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers) as response:
+                if response.status == 200:
+                    user_data = await response.json()
+                    user = user_data.get('results', [None])[0]
+                    if user:
+                        reply_message = f"User Information for {username}:\n"
+                        reply_message += f"Username: {user['username']}\n"
+                        reply_message += f"Name: {user['name']}\n"
+                        reply_message += f"Active: {user['is_active']}\n"
+                        reply_message += f"Last login: {user['last_login']}\n"
+                        reply_message += f"Email: {user['email']}\n"
+                        reply_message += "Attributes:\n"
+                        for key, value in user['attributes'].items():
+                            if not any(x in key.lower() for x in ["user", "pass", "settings"]):
+                                reply_message += f"  {key}: {value}\n"
+                        await ctx.send(reply_message)
+                    else:
+                        await ctx.send('No user found with the provided username.')
+                else:
+                    await ctx.send('Failed to fetch user attributes.')
