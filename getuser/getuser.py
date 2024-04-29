88c8f42dca83246a43df586e160c8b9193dbb950
Mrs Feathers <echo@furryrefuge.com>
2024-04-29 00:46:42 -0400

    Update and rename signup.py to getuser.py

--- getuser/getuser.py
+++ getuser/getuser.py
@@ -8,13 +8,13 @@ from redbot.core.utils.chat_formatting import humanize_number
 from redbot.core.utils.views import SimpleMenu
 
 
-class FRSignup(commands.Cog):
-    """Deliver Invite link."""
+class GetUser(commands.Cog):
+    """Get a users information from FR API"""
 
     def __init__(self, bot) -> None:
         self.bot = bot
 
-    @commands.command(name="signup")
+    @commands.command(name="getuser")
     async def signup(self, ctx):
         """Deliver Invite link."""
         await ctx.send("Your furry refuge account invite link is: https://auth.furryrefuge.com/if/flow/enrollment-invitation/?itoken=62bd2e38-b67b-46d2-9118-7636d54d48c6", delete_after=10)
