871b7d2cd3187f596d7a8f5494a8d8ec10ca23ff
Mrs Feathers <echo@furryrefuge.com>
2024-04-29 00:39:10 -0400

    Update __init__.py

--- getuser/__init__.py
+++ getuser/__init__.py
@@ -1,7 +1,7 @@
-from .signup import FRSignup
+from .getuser import GetUser
 
 __red_end_user_data_statement__ = "allows admin to get more info on a user"
 
 
 async def setup(bot):
-    await bot.add_cog(FRSignup(bot))
+    await bot.add_cog(GetUser(bot))
