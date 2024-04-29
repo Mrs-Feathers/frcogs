2347e49b9028574e3a4553ce5c692bde3fe5be12
Mrs Feathers <echo@furryrefuge.com>
2024-04-29 00:38:04 -0400

    Update __init__.py

--- getuser/__init__.py
+++ getuser/__init__.py
@@ -1,6 +1,6 @@
 from .signup import FRSignup
 
-__red_end_user_data_statement__ = "This cog allows you to request an invitation to make a Furry Refuge account!"
+__red_end_user_data_statement__ = "allows admin to get more info on a user"
 
 
 async def setup(bot):
