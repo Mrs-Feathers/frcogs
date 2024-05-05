bc522e545957e2186c4db31987c4241bbedc005f
Mrs Feathers <echo@furryrefuge.com>
2024-05-05 03:41:16 -0400

    Update getid.py

--- getid/getid.py
+++ getid/getid.py
@@ -25,7 +25,6 @@ class UserInfoPlugin(commands.Cog):
         embed.add_field(name="User ID", value=user.id, inline=True)
         embed.add_field(name="Account Created", value=user.created_at.strftime("%Y-%m-%d %H:%M:%S"), inline=True)
         embed.add_field(name="Bot?", value=user.bot, inline=True)
-        embed.set_thumbnail(url=user.avatar_url)
 
         await ctx.send(embed=embed)
 
