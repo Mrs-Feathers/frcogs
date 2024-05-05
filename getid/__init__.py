b525b0a2f97cf618821653c42d832e7ba0b91405
Mrs Feathers <echo@furryrefuge.com>
2024-05-05 03:36:21 -0400

    Update __init__.py

--- getid/__init__.py
+++ getid/__init__.py
@@ -1,7 +1,7 @@
-from .getid import get_user_info
+from .getid import UserInfoPlugin
 
 __red_end_user_data_statement__ = "Test to get debug info from user"
 
 
 async def setup(bot):
-    await bot.add_cog(get_user_info(bot))
+    await bot.add_cog(UserInfoPlugin(bot))
