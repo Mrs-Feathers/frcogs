07c95cc513bc54f7ce96e57fe1875b5d7e3af973
Mrs Feathers <echo@furryrefuge.com>
2024-05-05 03:33:54 -0400

    Update __init__.py

--- getid/__init__.py
+++ getid/__init__.py
@@ -1,7 +1,7 @@
-from .givexp import GiveXP
+from .getid import get_user_info
 
-__red_end_user_data_statement__ = "Give xp to a discord user"
+__red_end_user_data_statement__ = "Test to get debug info from user"
 
 
 async def setup(bot):
-    await bot.add_cog(GiveXP(bot))
+    await bot.add_cog(get_user_info(bot))
