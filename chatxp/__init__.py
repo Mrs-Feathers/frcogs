38774300777d26ec5c650bc8e09e95a1f2ba48c1
Mrs Feathers <echo@furryrefuge.com>
2024-05-25 03:44:57 -0400

    Update __init__.py

--- chatxp/__init__.py
+++ chatxp/__init__.py
@@ -1,7 +1,7 @@
-from .chatxp import GiveXP
+from .chatxp import ChatXP
 
 __red_end_user_data_statement__ = "This cog allows you earn xp!"
 
 
 async def setup(bot):
-    await bot.add_cog(GiveXP(bot))
+    await bot.add_cog(ChatXP(bot))
