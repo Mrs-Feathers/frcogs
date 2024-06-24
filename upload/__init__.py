9728150f5fc0cf608d895563aeff7e0168405f52
Mrs Feathers <echo@furryrefuge.com>
2024-06-24 13:58:03 -0400

    Update __init__.py

--- upload/__init__.py
+++ upload/__init__.py
@@ -1,7 +1,7 @@
-from .karma import KarmaPlugin
+from .upload import Upload
 
-__red_end_user_data_statement__ = "Give karma to a discord user"
+__red_end_user_data_statement__ = "Get a link to upload a photo"
 
 
 async def setup(bot):
-    await bot.add_cog(KarmaPlugin(bot))
+    await bot.add_cog(Upload(bot))
