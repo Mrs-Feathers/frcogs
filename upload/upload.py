2ca0a2b312d00f624ee2fe46702dee0f51f276a2
Mrs Feathers <echo@furryrefuge.com>
2024-06-24 13:56:23 -0400

    Update and rename karma.py to upload.py

---
+++ upload/upload.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import os
+import base64
+
+from redbot.core import commands, Config
+from redbot.core.utils.chat_formatting import humanize_number
+from redbot.core.utils.views import SimpleMenu
+
+class Upload(commands.Cog):
+    """Upload files using a user's Discord username as a token"""
+
+    def __init__(self, bot) -> None:
+        self.bot = bot
+        self.config = Config.get_conf(self, identifier=1234567890, force_registration=True)
+        self.config.register_global(token=None)
+
+    @commands.command(name="upload")
+    async def upload(self, ctx, username: str):
+        """Upload using the user's username as a token."""
+        if isinstance(ctx.channel, discord.DMChannel):
+            token = base64.b64encode(username.encode()).decode()
+            url = f"https://photovoter.furryrefuge.com/upload.php?token={token}"
+            await ctx.send(f"DO NOT SHARE THIS LINK! This is your token: {url}")
+        else:
+            await ctx.send("The !upload command can only be used in private messages with me!")
