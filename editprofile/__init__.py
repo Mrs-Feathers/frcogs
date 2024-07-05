f2ffdfeafb8f0b0a9c963d20169662adf0d63c72
Mrs Feathers <echo@furryrefuge.com>
2024-07-05 11:51:01 -0400

    Update __init__.py

--- editprofile/__init__.py
+++ editprofile/__init__.py
@@ -1,7 +1,7 @@
-from .signup import FRSignup
+from .editprofile import EditProfile
 
-__red_end_user_data_statement__ = "This cog allows you to request an invitation to make a Furry Refuge account!"
+__red_end_user_data_statement__ = "This cog allows you to request an link to edit your Furry Refuge account profile!"
 
 
 async def setup(bot):
-    await bot.add_cog(FRSignup(bot))
+    await bot.add_cog(EditProfile(bot))
