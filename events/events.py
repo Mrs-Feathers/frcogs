57f0fb94fb7e43fd689eda0eaefffa89637355bf
Mrs Feathers <echo@furryrefuge.com>
2024-07-05 22:14:53 -0400

    Update and rename editprofile.py to events.py

---
+++ events/events.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import aiohttp
+import os
+
+from redbot.core import commands, Config
+from redbot.core.utils.chat_formatting import humanize_number
+from redbot.core.utils.views import SimpleMenu
+
+
+class EventLink(commands.Cog):
+    """Deliver Events Calendar link."""
+
+    def __init__(self, bot) -> None:
+        self.bot = bot
+
+    @commands.command(name="events")
+    async def events(self, ctx):
+        """Deliver Events Calendar link."""
+        await ctx.send("Check out FR Events at: https://furryrefuge.com/events", delete_after=10)
