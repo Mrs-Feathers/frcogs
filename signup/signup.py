29dd944d9a30a8b99ddb6687de99313129ed482a
Mrs Feathers <echo@furryrefuge.com>
2024-07-26 10:37:50 -0400

    Update signup.py

--- signup/signup.py
+++ signup/signup.py
@@ -17,5 +17,5 @@ class FRSignup(commands.Cog):
     @commands.command(name="signup")
     async def signup(self, ctx):
         """Deliver Invite link."""
-        await ctx.send("Your furry refuge account invite link is: https://auth.furryrefuge.com/if/flow/enrollment-invitation/?itoken=62bd2e38-b67b-46d2-9118-7636d54d48c6", delete_after=10)
+        await ctx.send("Your furry refuge account invite link is: https://auth.furryrefuge.com/if/flow/enrollment-invitation/?itoken=62bd2e38-b67b-46d2-9118-7636d54d48c6")
         await ctx.send("Invite link sent! Use the command again to see the invite link again.")
