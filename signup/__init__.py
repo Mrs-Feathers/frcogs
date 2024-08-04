c9bf41a733d56f7e07313491446bd472f7f84f25
Mrs Feathers <echo@furryrefuge.com>
2024-08-04 17:24:10 -0400

    Update __init__.py

--- signup/__init__.py
+++ signup/__init__.py
@@ -1,7 +1,7 @@
-from .signup import FRSignup
+from .infocommands import FRInfo
 
-__red_end_user_data_statement__ = "This cog allows you to request an invitation to make a Furry Refuge account!"
+__red_end_user_data_statement__ = "This cog gives informational commands to help with the FR community!"
 
 
 async def setup(bot):
-    await bot.add_cog(FRSignup(bot))
+    await bot.add_cog(FRInfo(bot))
